3235c6588949c3af8f5c90af7827e7d8fe0e47dc
AlfaRiza <malfariza45@gmail.com>
2021-08-03 15:34:26 +0700

    ADD Model, Collections, Controller

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -4,6 +4,6 @@
     <h1>Halaman About</h1>
     <h1>{{ $name }}</h1>
     <p>{{ $email }}</p>
-    <img src="img/{{ $image }}" width="200" alt="{{ $name }}">
+    <img src="img/{{ $image }}" width="200" alt="{{ $name }}" class="img-thumbnail rounded-circle">
 @endsection
 
