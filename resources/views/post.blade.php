3875213cda2850cb90e06ca407554ebd1ebf2dd0
AlfaRiza <malfariza45@gmail.com>
2021-08-04 01:46:39 +0700

    ADD Post model

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,10 +1,10 @@
 @extends('layouts.main')
 
 @section('content')
-        <h5 class="card-header"><a href="/blog/{{ $post['slug'] }}">{{ $post['title'] }}</a></h5>
+        <h5 class="card-header"><a href="/blog/{{ $post->id }}">{{ $post->title }}</a></h5>
         <div class="card-body">
-            <h6 class="">{{ $post['author'] }}</h6>
-            <p class="card-text">{{ $post['body'] }}</p>
+            <!-- <h6 class="">{{ $post['author'] }}</h6> -->
+            {!! $post->body !!}
             <a href="/blog">Kembali</a>
         </div>
         </div>
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -5,10 +5,10 @@
     <div class="row">
         <div class="col">
         <div class="card mt-3">
-        <h5 class="card-header"><a href="/blog/{{ $post['slug'] }}">{{ $post['title'] }}</a></h5>
+        <h5 class="card-header"><a href="/blog/{{ $post->slug }}">{{ $post->title }}</a></h5>
         <div class="card-body">
-            <h6 class="">{{ $post['author'] }}</h6>
-            <p class="card-text">{{ \Illuminate\Support\Str::limit($post['body'], 25, ' ...') }}</p>
+            <!-- <h6 class="">{{ $post['author'] }}</h6> -->
+            <p class="card-text">{{ $post->excerpt }}</p>
         </div>
         </div>
         </div>
