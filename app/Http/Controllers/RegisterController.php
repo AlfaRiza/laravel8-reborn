24c389707cacc8e59d4e3a3fc1bb5c8db96079e9
AlfaRiza <malfariza45@gmail.com>
2021-08-18 22:51:17 +0700

    ADD register

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
+
 
 class RegisterController extends Controller
 {
@@ -12,4 +14,20 @@ class RegisterController extends Controller
             'title' => 'Register'
         ]);
     }
+
+    public function store(Request $request)
+    {
+        $validatedData = $request->validate([
+            'name' => 'required|max:255',
+            'username' => 'required|min:3|max:255|unique:users,username',
+            'email' => 'required|email:dns|unique:users,email|',
+            'password' => 'required|min:5|max:255',
+        ]);
+
+        $validatedData['password'] = bcrypt($validatedData['password']);
+
+        User::create($validatedData);
+        return view('/login')->with('Login Berhasil', 'success');
+    }
 }
+ 
